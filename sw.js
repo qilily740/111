a128855788f6b821cbde5a656711d236a800e349
qiliy <qiliy@qiliydeMacBook-8.local>
2026-09-16 22:41:04 +0800

    Preserve music page scroll when changing tracks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ideal-machine-shell-v20260916-desktop-float-1';
+const CACHE_NAME = 'ideal-machine-shell-v20260916-scroll-preserve-1';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
