560e4e4dfcb163c1c05293d39adb4df4e59d09a2
qiliy <qiliy@qiliydeMacBook-8.local>
2026-09-16 23:00:45 +0800

    Preserve queue panel scroll when selecting songs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ideal-machine-shell-v20260916-desktop-orb-click-1';
+const CACHE_NAME = 'ideal-machine-shell-v20260916-queue-scroll-1';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
