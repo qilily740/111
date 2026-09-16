2e8d17cb11fe86bba01199c827c290d4a13841ca
qiliy <qiliy@qiliydeMacBook-8.local>
2026-09-16 21:12:16 +0800

    make takeaway experience instant delivery

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ideal-machine-shell-v20260916-playback-ended-1';
+const CACHE_NAME = 'ideal-machine-shell-v20260916-takeaway-delivery-1';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
