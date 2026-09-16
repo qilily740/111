1270f4f620e5f534d1f931a78653b13f86ad9e60
qiliy <qiliy@qiliydeMacBook-8.local>
2026-09-16 22:34:30 +0800

    Make desktop lyrics draggable and minimizable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ideal-machine-shell-v20260916-takeaway-api-2';
+const CACHE_NAME = 'ideal-machine-shell-v20260916-desktop-float-1';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
